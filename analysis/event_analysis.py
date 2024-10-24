3fce531d31ddcadead96708e603b1d80fa90426c
Rohin Vaidya <rohinvaidya@outlook.com>
2024-10-24 16:26:56 -0400

    Update .gitignore

---
+++ analysis/event_analysis.py
@@ -0,0 +1,24 @@
+import config
+from typing import List
+
+class EventAnalysis:
+    """
+    Implements event analysis of GitHub
+    issues and outputs the result of that analysis.
+    """
+    
+    def __init__(self, analysis_args:List):
+        """
+        Constructor
+        """
+        # Parameter is passed in via command line (--user)
+        self.user:str = config.get_parameter('user')
+        self.label:str = config.get_parameter('label')
+        self.analysis_args = analysis_args # TODO Replace with config.get_parameter()
+    
+    def run(self):
+        pass
+
+if __name__ == '__main__':
+    # Invoke run method when running this module directly
+    EventAnalysis().run()
