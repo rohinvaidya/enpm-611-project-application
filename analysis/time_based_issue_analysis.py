d66570457ae002c85d5bced89b9a0e7e2dbad172
Rohin Vaidya <48517346+rohinvaidya@users.noreply.github.com>
2024-11-10 13:18:58 -0500

    Creator based analysis

--- analysis/time_based_issue_analysis.py
+++ analysis/time_based_issue_analysis.py
@@ -1,5 +1,6 @@
 import datetime as time
 from typing import List
+from numpy import average
 import pandas as pd
 import plotly.express as px
 
@@ -12,6 +13,8 @@ class TimeBasedIssueAnalysis:
     Implements time based issue analysis of closed
     GitHub issues and outputs the result of that analysis.
     """
+
+    pd.options.mode.copy_on_write = True
     
     def __init__(self):
         """
@@ -23,7 +26,6 @@ class TimeBasedIssueAnalysis:
     
     def run(self):
         issues:List[Issue] = DataLoader().get_issues()
-        no_of_issues = len(issues)
         open_issues = []
         closed_issues = []
 
@@ -33,17 +35,16 @@ class TimeBasedIssueAnalysis:
             elif issue.state == 'closed':
                 closed_issues.append(issue)
 
-        if self.state == 'open':
-            print('Number of open issues: ', len(open_issues))
-        elif self.state == 'closed':
-            print('Number of closed issues: ', len(closed_issues))
-            self.track_closed_issues(closed_issues)
+        print('Number of closed issues: ', len(closed_issues))
+
+        closed_issues_df = self.create_dataframe(closed_issues)
+
+        if self.user != None:
+            self.analyse_based_on_user(self.user, closed_issues_df)
         else:
-            print('Number of open issues: ', len(open_issues))
-            print('Number of closed issues: ', len(closed_issues))
+            self.analyse_closed_issues(closed_issues_df)
 
-
-    def track_closed_issues(self, closed_issues):
+    def create_dataframe(self, closed_issues):
         closed_issues_dict = dict()
         ids = []
         creators = []
@@ -74,86 +75,53 @@ class TimeBasedIssueAnalysis:
 
         closed_issues_df = pd.DataFrame(closed_issues_dict)
         closed_issues_df['time_taken'] = closed_issues_df['closed_time'] - closed_issues_df['creation_time']
-        
-        # Calculate time difference in days
-        closed_issues_df['time_diff'] = (closed_issues_df['closed_time'] - closed_issues_df['creation_time']).dt.days
 
-        # closed_issues_df['unique_id'] = list(pd.concat(closed_issues_df['creator'], closed_issues_df['issue_id']))
+        # Calculate time difference in days
+        closed_issues_df['time_diff_in_days'] = closed_issues_df['time_taken'].dt.days
+
+        # Calculate the time difference in months
+        closed_issues_df['time_diff_in_months'] = closed_issues_df['time_diff_in_days'].apply(self.get_approx_months)
 
         print(closed_issues_df.head())
         print()
+        
+        # Print the top 10 longest duration issues
+        print(closed_issues_df['time_diff_in_days'].nlargest(10))
 
-        closed_issues_sorted = closed_issues_df.sort_values(by='time_taken', ascending=False)
+        # Print the top 10 highest number of issues assigned to a single user
+        print(closed_issues_df['creator'].value_counts().nlargest(10))
 
-        print(closed_issues_sorted.head())
-        print(closed_issues_sorted.tail())
+        return closed_issues_df
 
-        # scaled_df = closed_issues_df.groupby(closed_issues_df["time_taken"]).value_counts().nlargest(10)
-
-        # print(scaled_df.head())
-
-        # Scatter plot for danceability vs streams
-        # fig = px.area(closed_issues_df,
-        #                 x='creator',
-        #                 y='time_taken',
-        #                 color='time_diff',
-        #                 hover_data=['issue_id', 'creator'],
-        #                 title='Time Taken to Close Issues',
-        #                 labels={'time_taken':'Time Taken'}
-        # )
-
-        # fig = px.bar(closed_issues_sorted[:100],
-        #                 x='unique_id',
-        #                 y='time_taken',
-        #                 color='time_taken',
-        #                 hover_data=['issue_id', 'creator', 'time_diff'],
-        #                 title='Time Taken to Close Issues',
-        #                 color_continuous_scale='Plasma',
-        #                 labels={'time_taken':'Time Taken'}
-        # )
-
-        # fig.show()
-
-        # Identify duplicates
-        duplicates = closed_issues_sorted.duplicated(keep=False)
-
-        # Create new dataframe with duplicates
-        df_duplicates = closed_issues_sorted[duplicates].copy()
-
-        df = closed_issues_sorted.drop_duplicates(subset=['creator'], keep='first')
-
-        print(df['time_diff'].value_counts().nlargest(10))
-
-        df['time_diff_in_months'] = df['time_diff'] / 30
-
-        # print(df_duplicates)
-
-        # fig = px.bar(df,
-        #         x='creator',
-        #         y='time_taken',
-        #         color='time_diff',
-        #         hover_data=['issue_id', 'creator', 'time_diff'],
-        #         title='Time Taken to Close Issues',
-        #         color_continuous_scale='Plasma')
-
-        # fig.show()
-
-        # fig = px.scatter(closed_issues_df,
-        #         x='creator',
-        #         y='time_taken',
-        #         color='time_diff',
-        #         hover_data=['issue_id', 'creator'],
-        #         title='Time Taken to Close Issues',
-        #         color_continuous_scale='Plasma')
-
-        # fig.show()
-
-        fig = px.area(df,
+    def analyse_closed_issues(self, closed_issues_df):
+        fig = px.area(closed_issues_df[:100],
                 x='creator',
                 y='time_diff_in_months',
-                color='time_diff_in_months',
+                color='time_diff_in_days',
                 hover_data=['labels'],
                 title='Time Taken to Close Issues')
+
+        fig.show()
+
+    def get_approx_months(self, time_diff):
+        return int(round(time_diff / 30, 0))
+    
+    def analyse_based_on_user(self, user, closed_issues_df):
+        # Create a dataframe with only information about the selected users
+        user_df = closed_issues_df[closed_issues_df['creator'] == user]
+
+        print(user_df)
+
+        average_time_taken = average(user_df['time_diff_in_days'])
+        print(f"The average time taken by user: {user} is {average_time_taken} days.")
+
+        fig = px.bar(user_df,
+            x='labels',
+            y='time_diff_in_days',
+            color='time_diff_in_days',
+            title=f"Time Taken by '{user}' to Close Issues (in days)",
+            labels={'time_diff_in_days':'Time Taken to Close Issues',
+                    'labels': 'Issue Label'})
 
         fig.show()
 
--- run.py
+++ run.py
@@ -7,6 +7,7 @@ the command line to run the analyses.
 
 import argparse
 
+from analysis.time_based_issue_analysis import TimeBasedIssueAnalysis
 import config as config
 from analysis.example_analysis import ExampleAnalysis
 from analysis.event_analysis import EventAnalysis
@@ -51,7 +52,7 @@ if args.feature == 0:
 elif args.feature == 1:
     IssueAnalysis(state = 'closed').run()
 elif args.feature == 2:
-    IssueAnalysis(state = 'open').run()
+    TimeBasedIssueAnalysis().run()
 elif args.feature == 3:
     EventAnalysis().run()
 else:
