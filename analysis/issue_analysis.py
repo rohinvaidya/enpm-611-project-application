325874663e20ffa539f5b4c18038da69ae6d27dd
Syed Muhammad Affan <affansyedmuhamma@gmail.com>
2024-11-10 23:08:16 -0500

    Bug fix

--- analysis/issue_analysis.py
+++ analysis/issue_analysis.py
@@ -133,9 +133,8 @@ def find_labels(self,issues):
     label_counts = defaultdict(int)
     # Iterate over each Issue object in the list
     for issue in issues:
-        if issue.state == self.state:
-            for label in issue.labels:
-                label_counts[label] += 1
+        for label in issue.labels:
+            label_counts[label] += 1
     sorted_labels = sorted(label_counts.items(), key=lambda x: x[1], reverse=True)
     return sorted_labels
 
